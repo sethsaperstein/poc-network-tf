6946fd5bdf17d9d0eda703227d371f12f7182178
sethsaperstein <sethsaper@gmail.com>
2021-11-14 20:55:11 -0500

    set default vars

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable vpc_cidr {
   type = string
+  default = "10.0.0.0/16"
 }
 
 variable pub_sub_cidr_block {
   type = string
+  default = "10.0.0.0/24"
 }
 
 variable prv_sub_cidr_block {
   type = string
+  default = "10.0.1.0/24"
 }
